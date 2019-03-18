1a86f97df973fb9b6088ffcf9a602075e0c64cad
Sophie Nichol <snichol00@stuy.edu>
2019-03-18 09:52:52 -0400

    reorganized and added comments

--- Merge.java
+++ Merge.java
@@ -6,19 +6,26 @@ public class Merge{
 
   //helper
   public static void mergesort(int[]data, int lo, int hi){
+    //if parameters unacceptable, stop method
     if (lo > hi){
       return;
     }
+    //make pivot the middle integer
     int p = (lo + hi) / 2;
+    //make make left and right sides of pivot
     int[] left = new int[data.length / 2];
     int[] right = new int[data.length - left.length];
-    mergesort(left, lo, p);
-    mergesort(right, p + 1, hi);
+    //fill left and right arrays
     for (int x = 0; x < left.length; x++){
       left[x] = data[x];
     }
     for (int x = 0; x < right.length; x++){
-      left[x] = data[x + p];
+      right[x] = data[x + p];
+    }
+    //break arrays even further
+    mergesort(left, 0, left.length - 1);
+    mergesort(right, p, right.length - 1);
+
     }
   }
 }
