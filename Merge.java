7a7f62a74858cfa203d055d17eab84c92025a8a9
Sophie Nichol <snichol00@stuy.edu>
2019-03-18 23:19:08 -0400

    trying to resolve stack overflow error

--- Merge.java
+++ Merge.java
@@ -14,6 +14,7 @@ public class Merge{
     int p = (lo + hi) / 2;
     //make make left and right sides of pivot
     int[] left = new int[data.length / 2];
+    //if an odd length, right side will be larger
     int[] right = new int[data.length - left.length];
     //fill left and right arrays
     for (int x = 0; x < left.length; x++){
@@ -24,20 +25,22 @@ public class Merge{
     }
     //break arrays even further
     mergesort(left, 0, left.length - 1);
-    mergesort(right, p, right.length - 1);
+    mergesort(right, 0, right.length - 1);
     //keeps track of the left and right indices
     int lidx = 0;
     int ridx = 0;
+    //keeps track of data index
+    int idx = 0;
     //loops through data to store sorted left and right arrays
-    while (idx < hi){
+    while (idx < data.length){
       try{
         if (left[lidx] < right[ridx]){
           data[idx] = left[lidx];
           lidx++;
-          idx++
+          idx++;
           //data[2 * idx + 1] = right[idx];
         }
-        if (right[ridx] < left[lidx]){
+        else{
           data[idx] = right[ridx];
           ridx++;
           idx++;
@@ -45,12 +48,12 @@ public class Merge{
         }
       }
       catch(IndexOutOfBoundsException e){
-        while(lidx < left.length){
+        if (lidx < left.length){
           data[idx] = left[lidx];
           lidx++;
           idx++;
         }
-        while(ridx < right.length){
+        else if (ridx < right.length){
           data[idx] = right[ridx];
           ridx++;
           idx++;
